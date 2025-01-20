545a17f45ad8cfc4f50b61cff1f5ec480afaa257
JacobWesthoff04 <mzufnz@gmail.com>
2025-01-20 16:32:50 -0600

    finished main

--- HighScores.java
+++ HighScores.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class HighScores {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your initials: ");
+        String initials = scanner.nextLine();
+
+        System.out.print("Enter your score: ");
+        int score = scanner.nextInt();
+
+        scanner.close();
+
+        Player player = new Player(initials, score);
+
+        System.out.println("Player: " + player.getInitials() + "," + player.getScore());
     }
 }
 
